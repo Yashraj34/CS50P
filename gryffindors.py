a4ff900e86b537c33ec7e1f87f843731b26369de
Yashraj34 <89093009+Yashraj34@users.noreply.github.com>
2025-06-10 08:38:17 +0530

    Add files via upload

---
+++ gryffindors.py
@@ -0,0 +1,8 @@
+students=["Hermione","Harry","Ron"]
+
+gryffindors=[{"name": student,"house":"Gryffindor"}for student in students]
+
+#for student in students:
+ #-   gryffindors.append({"name": student,"house":"Gryffindor"})
+
+print(gryffindors)
---
+++ sleep.py
@@ -0,0 +1,8 @@
+def main():
+    n=int(input("what is n? "))
+    for i in range(n):
+        print("🐑"*i)
+
+if __name__=="__main__":
+    main()
+
